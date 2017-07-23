15993bfb28a4a6d5a0578d876b270ba4a20845fd
netjargon <philswhitejr@outlook.com>
2017-07-23 18:09:20 -0400

    added logic to bypass configuration

--- utilities/jobs.rb
+++ utilities/jobs.rb
@@ -13,29 +13,38 @@ end
 # a password but more preferable and secure, an API token can be used to authenticate to the Jenkins
 # server.
 
-puts "Please enter a server ip"
-server_ip = gets.chomp
+puts "Do you need to configure jobs.rb?"
+answer = gets.chomp
+case answer
 
-puts "Please, enter a server port"
-server_port = gets.chomp
+when "yes"
+    puts "Please enter a server ip"
+    server_ip = gets.chomp
 
-puts "Please enter your username"
-username = gets.chomp
+    puts "Please, enter a server port"
+    server_port = gets.chomp
 
-puts "Please, enter your password or API Token which will be added to a plaintext config file"
-password = get_password()
+    puts "Please enter your username"
+    username = gets.chomp
 
-
-
- config['username'] = username
- config['password'] = password
- config['server_ip'] = server_ip
- config['server_port'] = server_port
+    puts "Please, enter your password or API Token which will be added to a plaintext config file"
+    password = get_password()
 
 
-File.open('config.yml', 'w') {
-    |f| f.write config.to_yaml
-}
+
+    config['username'] = username
+    config['password'] = password
+    config['server_ip'] = server_ip
+    config['server_port'] = server_port
+
+
+    File.open('config.yml', 'w') {
+        |f| f.write config.to_yaml
+    }
+
+when "no"
+    puts "#{answer} is the answer and we will continue"
+end
 
 jenkins = JenkinsApi::Client.new(config)
 
