60a3a03dd84f683ab789b0008672b29aa9be53ce
netjargon <philswhitejr@outlook.com>
2017-07-23 17:24:41 -0400

    adding feature to get server ip and port from user and update config.yml

--- utilities/jobs.rb
+++ utilities/jobs.rb
@@ -2,6 +2,7 @@ require 'jenkins_api_client'
 require 'highline/import'
 require 'encryptor'
 require 'openssl'
+require 'base64'
 
 config = YAML.load_file('config.yml')
 
@@ -9,39 +10,43 @@ def get_password(prompt="Enter Password")
     ask(prompt) {|q| q.echo = false}
 end
 
+# a password but more preferable and secure, an API token can be used to authenticate to the Jenkins
+# server.
+
+puts "Please enter a server ip"
+server_ip = gets.chomp
+
+puts "Please, enter a server port"
+server_port = gets.chomp
+
+puts "Please enter your username"
+username = gets.chomp
+
+puts "Please, enter your password or API Token which will be added to a plaintext config file"
 password = get_password()
-# Need to get if Jenkins is able to receive an encrypted string of a user password
 
-# cipher = OpenSSL::Cipher.new('aes-256-gcm')
-# cipher.encrypt
-# secret_key = cipher.random_key
-# iv = cipher.random_iv
-# salt = SecureRandom.random_bytes(16)
-# encrypted_value = Encryptor.encrypt(value: password, key: secret_key, iv: iv, salt: salt)
-# decrypted_value = Encryptor.decrypt(value: encrypted_value, key: secret_key, iv: iv, salt: salt)
 
-config['password'] = password
+
+ config['username'] = username
+ config['password'] = password
+ config['server_ip'] = server_ip
+ config['server_port'] = server_port
+
 
 File.open('config.yml', 'w') {
     |f| f.write config.to_yaml
 }
 
-puts "Please enter your username"
-#userName = gets.chomp
-
-
 jenkins = JenkinsApi::Client.new(config)
 
 job = JenkinsApi::Client::Job.new(jenkins)
 
-# #while 
+puts "Here are a list of jobs that are currently available"
+puts job.list_all
 
-    puts "Here are a list of jobs that are currently available"
-    puts job.list_all
+puts "Enter the name of the job you want to run"
+jobName = gets.chomp
 
-    puts "Enter the name of the job you want to run"
-    jobName = gets.chomp
+puts "testing build job #{jobName}"
+job.build(jobName)
 
-    puts "testing build job #{jobName}"
-    job.build(jobName)
-# #end
